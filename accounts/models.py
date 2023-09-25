484783e69f523b3fff475ed94883eb0896de2b6a
Ghada <ghada.abuelzalaf@gmail.com>
2023-09-25 22:30:10 +0300

    added accounts directory for user signup

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    address = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.email
